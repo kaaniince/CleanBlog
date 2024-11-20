f3de548257f103dd8b412706974221be0b6515b9
Kaan Ince <kaaniince@outlook.com>
2024-11-20 14:05:59 +0300

    template engine added

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+//Template Engine
+app.set('view engine', 'ejs');
 
 //Middleware
 app.use(express.static('public'));
 
+//Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
